0a515eda8092a75c1ec99bfef7b3a03740b44a4c
Noman Syed <100182533@student.burnley.ac.uk>
2024-02-02 09:39:56 +0000

    Updated code

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -3,43 +3,106 @@
 namespace ConsoleAppProject.App01
 {
     /// <summary>
-    /// Please describe the main features of this App
+    /// This App has been designed & created to simply use the three main distance measurement units to let the user enter a distance in numbers and the application will then convert the distance to the measurement unit chosen by the user.
     /// </summary>
     /// <author>
-    /// Derek version 0.1
+    /// Noman Syed version 1
     /// </author>
     public class DistanceConverter
     {
-        private double miles;
-        private double feet;
+        private double distance;
+        private string inputUnit;
+        private string outputUnit;
 
-        /// <summary>
-        /// 
-        /// </summary>
+        public const double METERS_IN_MILE = 1609.34;
+        public const int FEET_IN_MILE = 5280;
+
         public void Run()
         {
-            InputMiles();
-            CalculateFeet();
-            OutputFeet();
+            OutputHeading();
+            InputDistance();
+            InputUnits();
+            CalculateDistance();
+            OutputDistance();
         }
 
-        /// <summary>
-        /// Prompt the user to enter the distance in miles
-        /// Input the miles as a double number
-        /// </summary>
-        private void InputMiles()
+        private void OutputHeading()
         {
-            Console.Write("Please enter the number of miles you would like to get converted");
-            string value = Console.RedLine();
-            miles = Convert.ToDouble(value);
+            Console.WriteLine("\n------------------------------------");
+            Console.WriteLine("        Distance Converter         ");
+            Console.WriteLine("          by Noman Syed             ");
+            Console.WriteLine("------------------------------------\n");
         }
 
-        private void CalculateFeet()
+        private void InputDistance()
         {
+            int maxAttempts = 3;
+            int attempts = 0;
+
+            do
+            {
+                Console.Write("Please enter the distance in numbers: ");
+                string value = Console.ReadLine();
+
+                try
+                {
+                    distance = Convert.ToDouble(value);
+                    break; // Break out of the loop if conversion is successful
+                }
+                catch (FormatException)
+                {
+                    attempts++;
+                    Console.WriteLine("Invalid input. Please enter a valid number.");
+
+                    if (attempts >= maxAttempts)
+                    {
+                        Console.WriteLine($"Exceeded maximum attempts ({maxAttempts}). Exiting program.");
+                        Environment.Exit(0); // Exit the program if max attempts are reached
+                    }
+                }
+            } while (true);
         }
 
-        private void OutputFeet()
+        private void InputUnits()
         {
+            Console.Write("Enter the input unit (miles, feet, meters): ");
+            inputUnit = Console.ReadLine().ToLower();
+
+            Console.Write("Enter the output unit (miles, feet, meters): ");
+            outputUnit = Console.ReadLine().ToLower();
+        }
+
+        private void CalculateDistance()
+        {
+            if (inputUnit == "miles" && outputUnit == "feet")
+            {
+                distance *= FEET_IN_MILE;
+            }
+            else if (inputUnit == "miles" && outputUnit == "meters")
+            {
+                distance *= METERS_IN_MILE;
+            }
+            else if (inputUnit == "feet" && outputUnit == "miles")
+            {
+                distance /= FEET_IN_MILE;
+            }
+            else if (inputUnit == "feet" && outputUnit == "meters")
+            {
+                distance /= 3.281; // 1 foot = 0.3048 meters
+            }
+            else if (inputUnit == "meters" && outputUnit == "miles")
+            {
+                distance /= METERS_IN_MILE;
+            }
+            else if (inputUnit == "meters" && outputUnit == "feet")
+            {
+                distance *= 3.281; // 1 meter = 3.281 feet
+            }
+        }
+
+        private void OutputDistance()
+        {
+            Console.WriteLine($"{distance} {outputUnit}");
         }
     }
 }
